5ca399c956339131144fa32e4b2950ffce43cf4d
alex-fossa-204 <shift.kite11@yandex.ru>
2022-05-04 17:20:37 +0300

    [add]: repository implementation added

--- src/main/java/by/andersen/laboratory/microservice/users/repository/RoleRepository.java
+++ src/main/java/by/andersen/laboratory/microservice/users/repository/RoleRepository.java
@@ -1,4 +1,12 @@
 package by.andersen.laboratory.microservice.users.repository;
 
-public interface RoleRepository {
+import by.andersen.laboratory.microservice.users.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository <Role, Long> {
+
+    Role findRoleByRoleName(String roleName);
+
 }
