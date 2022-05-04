f66877ce05fab97a3bad2d348b1679799d8e6a0f
alex-fossa-204 <shift.kite11@yandex.ru>
2022-05-04 17:21:20 +0300

    [add]: utils and properties implementation added

--- src/main/java/by/andersen/laboratory/microservice/users/utils/RandomUidGenerator.java
+++ src/main/java/by/andersen/laboratory/microservice/users/utils/RandomUidGenerator.java
@@ -1,4 +1,15 @@
 package by.andersen.laboratory.microservice.users.utils;
 
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
 public class RandomUidGenerator {
+
+    public Long generateRandomTransferUid() {
+        return UUID.randomUUID().getMostSignificantBits() >>> 16;
+    }
+
+
 }
